6ecd0e56ef65da1bb02b05620eba657cba86585d
Ali Kouhfar <alikouhfr@gmail.com>
2026-05-04 21:44:07 +0330

    fix: Open Source Map

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+
+  productionBrowserSourceMaps: true,
+
   experimental: {
     serverActions: {
       bodySizeLimit: '100mb',
