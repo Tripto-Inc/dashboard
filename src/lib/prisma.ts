8a39821358aa9c658070663dd377b358ad64bfdb
Ali Kouhfar <alikouhfr@gmail.com>
2026-05-04 21:57:01 +0330

    fix: Fix Build Issues

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg';
-import "dotenv/config";
-
-console.log(process.env.DATABASE_URL)
 
 const connectionString = `${process.env.DATABASE_URL}`
 
