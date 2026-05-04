c51b589d5e9f61c23d4fd268f705a2a84f2801a2
Ali Kouhfar <alikouhfr@gmail.com>
2026-05-04 22:04:17 +0330

    fix: Fix Build Issues

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+if (!connectionString) {
+    throw new Error("DATABASE_URL is missing in production");
+}
 
-export { prisma };
+const adapter = new PrismaPg({ connectionString });
+
+export const prisma = new PrismaClient({ adapter });
