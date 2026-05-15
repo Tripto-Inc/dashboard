2a0a94924d9a64bc1845cdb5d147e77ae3ac2128
Ali Kouhfar <alikouhfr@gmail.com>
2026-05-15 09:46:08 +0330

    fix: Add Google To Remote Patterns for Next Image

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,12 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: process.env.NODE_ENV === 'development',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 };
 
